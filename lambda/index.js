9201a37eabd4a8cbe3f4e3a117f95beb1c8e9ccc
aaron <huang.aaron03@gmail.com>
2019-12-02 00:58:39 -0800

    create logging statement

--- lambda/index.js
+++ lambda/index.js
@@ -154,6 +154,7 @@ const YesIntentHandler = {
         switch (yesIntent) {
             case YES_INTENTS.LAST_DRINK_CONFIRMATION:
                 try{
+                    if('lastDrink' in persistentAttributes) console.log('lastDrink is ' + persistentAttributes.lastDrink)
                     let drinkObject = persistentAttributes.lastDrink
                     await createDrink(drinkObject.tea, drinkObject.sugar. drinkObject.ice)
                     let drinkString = persistentAttributes.lastDrink.string
